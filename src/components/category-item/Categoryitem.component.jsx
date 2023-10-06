cdff8bda7224e41138b44e0c8e53fcf45c564cd9
pranjalsingh05 <pranjalsingh.ishu@gmail.com>
2023-10-06 17:00:48 +0530

    Added links to home page categories

--- src/components/category-item/Categoryitem.component.jsx
+++ src/components/category-item/Categoryitem.component.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import {useNavigate} from "react-router-dom"
 
 const Categoryitem= ({category}) => {
+    const {title,id,imageUrl,route}=category
+    const navigate=useNavigate();
+    const shopHandler=()=>{
+        navigate(`${route}`)
+    }
   return (
       <div
-          key={category.id}
+         onClick={shopHandler}
+          key={id}
           className=" min-w-[30%] h-[240px] flex flex-1 items-center justify-center border border-black mt-0 mr-7.5 mb-15 ml-0 overflow-hidden group transition-transform duration-600 ease-in-out transform-gpu scale-100 hover:scale-110 hover:cursor-pointer hover:opacity-90"
       >
           <div
               className="w-full h-full bg-cover bg-center bg-no-repeat transition-transform duration-600 ease-in-out transform-gpu scale-100 group-hover:scale-110"
               style={{
-                  backgroundImage: `url(${category.imageUrl})`,
+                  backgroundImage: `url(${imageUrl})`,
               }}
           />
           <div className="h-[90px] px-[25px] flex flex-col items-center justify-center border border-black bg-white opacity-70 absolute">
               <h1 className="font-bold text-2xl text-[#4a4a4a] mb-1">
-                  {category.title}
+                  {title}
               </h1>
               <p className="font-light text-base">Shop Now</p>
           </div>
--- src/routes/home/Home.component.jsx
+++ src/routes/home/Home.component.jsx
@@ -13,37 +13,11 @@ import CheckOut from '../checkout/CheckOut.component';
 
 const Home = () => {
   
-  const categories = [
-          {
-              id: 1,
-              title: 'hats',
-              imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          },
-          {
-              id: 2,
-              title: 'jackets',
-              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          },
-          {
-              id: 3,
-              title: 'sneakers',
-              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          },
-          {
-              id: 4,
-              title: 'womens',
-              imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          },
-          {
-              id: 5,
-              title: 'mens',
-              imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          },
-        ]
+ 
   return (
       <Routes>
           <Route path="/" element={<Navigation />}>
-              <Route index element={<Directory categories={categories} />} />
+              <Route index element={<Directory  />} />
               <Route path="/shop/*" element={<Shop />} />
               <Route path="/signin" element={<SignIn />}/>
               <Route path="/checkout" element={<CheckOut />} /> 
